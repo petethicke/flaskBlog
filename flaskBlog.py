339474c956e9e6b2798fbe19b273e21cee7374e8
Peter Thicke <peterbthicke@gmail.com>
2015-07-22 12:43:26 -0700

    created database

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
+from contextlib import closing
 
 DATABASE = '/tmp/flaskBlog.db'
 DEBUG = True
@@ -17,3 +18,10 @@ def connect_db():
 
 if __name__ == '__main__':
     app.run()
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+        
