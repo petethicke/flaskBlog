0900f7ecc8510454aeb67cc466c95c9d1de7d012
Peter Thicke <peterbthicke@gmail.com>
2015-07-22 22:24:59 -0700

    database connection

--- flaskBlog.py
+++ flaskBlog.py
@@ -24,4 +24,13 @@ def init_db():
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
-        
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
